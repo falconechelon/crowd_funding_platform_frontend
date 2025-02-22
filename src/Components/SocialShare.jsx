53e9fe47b7e352b48b6596543e9848ff52103926
falconechelon <rmondal030697@gmail.com>
2025-02-22 11:38:42 +0530

    Initial commit

---
+++ src/Components/SocialShare.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedinShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+} from 'react-share';
+import './styles/SocialShare.css';
+
+const SocialShare = ({ url, title }) => {
+  return (
+    <div className="social-share">
+      <FacebookShareButton url={url} quote={title}>
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+      <TwitterShareButton url={url} title={title}>
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+      <LinkedinShareButton url={url} title={title}>
+        <LinkedinIcon size={32} round />
+      </LinkedinShareButton>
+    </div>
+  );
+};
+
+export default SocialShare;
